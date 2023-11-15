23fc9ab854ceded17b27302f746e8121ccf5f38d
Jáchym Toušek <enumag@gmail.com>
2023-11-15 10:40:49 +0100

    Hou+

--- load.php
+++ load.php
@@ -4,8 +4,9 @@ require __DIR__ . '/bootstrap.php';
 
 dibi::query('TRUNCATE [voices]');
 
-$files = \Nette\Utils\Finder::findFiles('s*.txt')
+$files = \Nette\Utils\Finder::findFiles('*.txt')
     ->exclude('s00.txt')
+    ->exclude('hou-plus.txt')
     ->in(__DIR__ . '/dev');
 
 foreach ($files as $file) {
